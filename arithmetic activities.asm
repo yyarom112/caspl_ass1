f5a9b36bf2fa3c6da9fff78cb7c72965e18f9652
yyarom112 <yaromy@post.bgu.ac.il>
2018-03-24 10:56:06 +0300

    Add files via upload
    add arithmetic act but only for int not for bignum

---
+++ arithmetic activities.asm
@@ -0,0 +1,37 @@
+Add:
+                    push rbp				;backup RBP
+                    move rbp, rsp			;reset RBP to current RSP
+                    move rbp, rsp			;reset RBP to current RSP
+                    adc rdi, rsi                        ;execute
+                    move rax, rdi			;put return value into rax
+                    move rsp, rbp			;move RBP to RSP
+                    pop rbp				;restore RBP
+                    RET	                                ;return from the function
+
+Subtract: ;this function do rdi(first arg)-rsi(second arg)
+                    push rbp				;backup RBP
+                    move rbp, rsp			;reset RBP to current RSP
+                    move rbp, rsp			;reset RBP to current RSP
+                    sbb rdi, rsi                        ;execute
+                    move rax, rdi			;put return value into rax
+                    move rsp, rbp			;move RBP to RSP
+                    pop rbp				;restore RBP
+                    RET	 
+Multiply:
+                    push rbp				;backup RBP
+                    move rbp, rsp			;reset RBP to current RSP
+                    move rbp, rsp			;reset RBP to current RSP
+                    imult rdi, rsi                      ;execute--for bignum need to change
+                    move rax, rdi			;put return value into rax
+                    move rsp, rbp			;move RBP to RSP
+                    pop rbp				;restore RBP
+                    RET	 
+Divide:
+                    push rbp				;backup RBP
+                    move rbp, rsp			;reset RBP to current RSP
+                    move rbp, rsp			;reset RBP to current RSP
+                    iDIV rdi, rsi                       ;execute--for bignum need to change
+                    move rax, rdi			;put return value into rax
+                    move rsp, rbp			;move RBP to RSP
+                    pop rbp				;restore RBP
+                    RET	 
